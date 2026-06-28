d63fd263201fc1e71f50b3f25bf5f3a00ba048dd
vistaprods-projects <vistaprod.io@gmail.com>
2026-06-28 19:26:17 +0200

    fix: Add cache-busting query parameter to service worker registration

--- js/app.js
+++ js/app.js
@@ -23,7 +23,7 @@ class App {
 
         // Registrar Service Worker (PWA)
         if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('./sw.js')
+            navigator.serviceWorker.register('./sw.js?v=6')
                 .then(reg => console.log('[App] SW Registered', reg.scope))
                 .catch(err => console.warn('[App] SW Error', err));
         }
